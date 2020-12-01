d3d2ec7acbee170a681631a493641d7c0cced535
Ankit Satpati <67791452+Ankit284-boop@users.noreply.github.com>
2020-12-01 18:51:06 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fc642df08b41a001fec9df5",
+				"5fc642df08b41a001fec9df7"
+			]
+		},
+		"5fc642df08b41a001fec9df5": {
+			"id": "5fc642df08b41a001fec9df5",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fc642df08b41a001fec9df7": {
+			"id": "5fc642df08b41a001fec9df7",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
